f4a29018ac7a97b4e9b03c0a0f9bb345af4080e5
Kaizad <kaizad.patel@pnnl.gov>
2020-01-28 07:42:45 -0800

    trying to parse datetime

--- 1-moisture.R
+++ 1-moisture.R
@@ -10,8 +10,12 @@ moisture = sapply(list.files(path = "moisture/",pattern = "*.csv", full.names = 
 
 moisture2 = 
   moisture %>% 
-  dplyr::mutate(DATETIME = parse_date_time(Datetime, orders="mdy HMS"),
-                datetime = mdy_hms(Datetime))
+  dplyr::mutate(
+# this parses only those formats with time in HH:MM (24-hour) format. AM/PM formats are NA
+    Datetime2 = parse_datetime(Datetime, "%m%.%d%.%Y%T"),
+# this tries to parse everythng, but only does II:MM:SS AM/PM correctly. the HH:MM (24-hour) is parsed wrong
+    datetime = mdy_hms(Datetime))
+# so for now, parse both ways, and then select only the relevant cells to make a new combined column.
 
 ggplot(moisture, aes(x = Datetime, y = Moisture_m3_m3))+
   geom_point()
